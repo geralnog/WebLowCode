3191f786be8c9cd6e595d259033c2a753f44339b
geralnog <geralnog@gmail.com>
2026-02-25 15:11:06 +0000

    aula25.02

--- js/aula.js
+++ js/aula.js
@@ -40,3 +40,91 @@ let cutIndex = fullName.indexOf(" ");
 let firstName = fullName.slice(0, cutIndex);
 alert(firstName); */
 
+/* //primeira linha de código JS
+let firstCode = "hello world";
+let className = "Low Code";
+let number = 9;
+ 
+//reassignar um novo valor à variável
+className = "Design UI/UX";
+ 
+//concatenar variáveis
+alert(className+': '+firstCode+'!');
+ 
+//forma alternativa de concatenar variáveis
+alert(`a turma é ${className}: ${firstCode}!`);
+  */
+
+/* //alert
+
+alert('olá sou um alerta!')
+ */
+/* //console.log()
+console.log('estou loggado na consola e sou amigo dos programadores')
+ */
+
+
+/* let userName = prompt('Qual é o seu nome?');
+
+console.log('olá programador ' + userName);
+
+
+
+console.log("Olá " + firstName + " " + lastName); */
+
+
+/* switch (weekDay) {
+  case "segunda":
+  case "segunda-feira":
+    alert("é segunda");
+    break;
+  case "terça":
+  case "terça-feira":
+    alert("é terça");
+    break;
+  case "quarta":
+  case "quarta-feira":
+    alert("é quarta");
+    break;
+  case "quinta":
+  case "quinta-feira":
+    alert("é quinta");
+    break;
+  case "sexta":
+  case "sexta-feira":
+    alert("é sexta");
+    break;
+  case "sábado":
+  case "domingo":
+    alert("é fim de semana");
+    break;
+  default:
+    alert("dia inválido");
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
+/* let weekDay = prompt('Que dia da semana é hoje?').toLowerCase().trim();
+
+if (weekDay == 'segunda') {
+    alert('Espoca segunda')
+}else
+    alert('Não é segunda')
+
+    if (weekDay == "segunda" || weekDay == "segunda-feira") {
+  alert("força para esta semana!");
+} else if (weekDay == "quarta" || weekDay == "quarta-feira") {
+  alert("chegámos ao meio da semana");
+} else {
+  alert("já faltou mais para o fim de semana");
+} */
+
--- js/exercicios.js
+++ js/exercicios.js
@@ -1,5 +1,82 @@
-let myLuckyNumber = 7;
+/* let myLuckyNumber = 7;
 
 let myName = 'Geraldo Nogueira';
 
-alert(myName + ': ' + myLuckyNumber)
+alert(myName + ': ' + myLuckyNumber) */
+
+
+/* 
+const Word = "skateboard";
+
+let newWord = Word.slice(5);
+let facialHair = newWord.replace("o", "e");
+console.log(facialHair); 
+
+ou
+
+const WORD = 'skateboard';
+let facialHair = WORD.slice(5).replace('o','e');
+alert(facialHair); */
+
+/* let firstName = prompt('Qual é o seu primeiro nome?');
+let lastName = prompt('Qual é o seu último nome?');
+
+
+
+
+
+/*   Peça ao utilizador para digitar uma nota de 0 a 100 usando prompt e if/else:
+ 
+Verifique:
+Se for menor que 50 → "Reprovado"
+Se for entre 50 e 79 → "Aprovado"
+Se for 80 ou mais → "Aprovado com Distinção"
+Mostre o resultado usando alert.
+  */
+ 
+/* let nota = prompt('Digite uma nota de 0 a 100');
+if (nota < 50) {
+    alert ('Reprovado')
+}else if (nota >= 50 && nota < 80) {
+    alert ('Aprovado')
+}else if (nota >= 80) {
+    alert ('Aprovado com Distinção')
+}else {
+    alert('Nota inválida')
+} */
+
+/* 2. Crie um sistema que:
+ 
+Peça o nome do utilizador
+Peça a idade
+ 
+Se for maior de idade, mostre um menu com indicações personalizadas (tratando pelo nome):
+Ver saldo: simule com um alerta 'tem 1.0000 de euros'
+Fazer levantamentos: pergunte ao utilizador o valor a levantar. No final do levantamento, indique quanto ficou em conta.
+Depositar dinheiro: lance um alerta a indicar 'depósito de 600euros efectuado!'
+Se for menor, mostre: "Acesso negado */
+
+let = userName = prompt('Qual é o seu nome?');
+let idade = prompt('Qual é a sua idade?');
+
+if (idade >= 18) {
+
+
+    alert('Veja as opções do menu');   
+    let opcao = prompt('Escolha uma opção: 1 - Ver saldo, 2 - Levantar dinheiro, 3 - Depositar dinheiro');
+    switch (opcao) {
+        case '1':
+            alert('Você tem 1.0000 de euros');
+            break;
+        case '2':
+            let valorLevantamento = prompt('Quanto deseja levantar?');
+            alert('Você levantou ' + valorLevantamento + ' euros. Agora tem ' + (10000 - valorLevantamento) + ' euros.');
+            break;
+        case '3':
+            alert('Depósito de 600 euros efectuado!');
+            break;
+        default:
+            alert('Opção inválida');
+    }
+        
+        }
